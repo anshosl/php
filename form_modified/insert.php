5c4d5f4d3f07f8e708cbb006590a06a44ce2a089
anshosl <140603776+anshosl@users.noreply.github.com>
2023-08-04 16:27:17 +0530

    Update insert.php

--- form_modified/insert.php
+++ form_modified/insert.php
@@ -1,18 +1,23 @@
-<?php 
+<?php
 
-include ("connection.php");
-if(isset($_POST["submit"])) {
-$fname = $_POST['fname'];
-$email = $_POST['email'];
-$lname = $_POST['lname'];
+include("connection.php");
+if (isset($_POST["submit"])) {
+    $fname = $_POST['fname'];
+    $email = $_POST['email'];
+    $lname = $_POST['lname'];
 
-$query = "INSERT INTO form(fname,lname,email) VALUES ('{$fname}','{$lname}' ,'{$email}')";
-$data = mysqli_query($conn,$query);
-if($data){
-    echo "inserted";
+    $sql1 = "SELECT * FROM form where email='{$email}'";
+    $result = mysqli_query($conn, $sql1);
+    if(mysqli_num_rows($result)){
+        header("Location: http://localhost/php/form/update.php?email=$email&fname=$fname&lname=$lname");
+    }
+    else{
+        $query = "INSERT INTO form(fname,lname,email) VALUES ('{$fname}','{$lname}' ,'{$email}')";
+        $data = mysqli_query($conn, $query);
+        if ($data) {
+            echo "inserted";
+        } else {
+            echo "not inserted";
+        }
+    }
 }
-else{
-    echo "not inserted";
-}
-}
-?>
